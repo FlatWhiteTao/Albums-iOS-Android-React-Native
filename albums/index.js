786380edc786317fd1e9a8fb5d98b07da740a289
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-03 20:42:23 +1100

    Add a albumList component to contain a list of albums data

--- albums/index.js
+++ albums/index.js
@@ -3,13 +3,18 @@
 // import a library to help create a component
 import React from 'react';
 import {
-  AppRegistry
+  AppRegistry,
+  View
 } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
