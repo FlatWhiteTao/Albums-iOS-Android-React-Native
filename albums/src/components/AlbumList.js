f8be3e76834d37be6c00a1f7ff9d11edf2795093
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-03 22:53:21 +1100

    Connect a sample data center using fecth api
    Axios has an unsolvable bug

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+//import axios from 'axios'
 
 class AlbumList extends Component {
+  //init loading some data when the albumlist component rendered
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => console.log(response));
+    //axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      //.then((response) => console.log(response));
+}
+
   render() {
     return (
       <View>
